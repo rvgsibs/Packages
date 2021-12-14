088a4b1d1820fd2f3a9c11e8acdbf4d22665a914
Rodrigo Sousa <rodrigovgsousa@gmail.com>
2021-12-14 15:43:56 -0300

    Add project files.

---
+++ CrossCutting/Attributes/HttpStatusCodeAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CrossCutting.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple =false, Inherited =true)]
+    public class HttpStatusCodeAttribute : Attribute
+    {
+        private int statusCode;
+
+        public HttpStatusCodeAttribute(int statusCode)
+        {
+            this.statusCode = statusCode;
+        }
+
+        public int StatusCode { get => statusCode; }
+    }
+}
---
+++ Intercept/Intercept.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Intercept
+{
+    public class Intercept : ControllerBase
+    {
+        [NonAction]
+        public IActionResult Execute(Func<object> func)
+        {
+            try
+            {
+                var result = func();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [NonAction]
+        public async Task<IActionResult> ExecuteAsync(Func<Task<object>> func)
+        {
+            try
+            {
+                var result = await func();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [NonAction]
+        public Guid GetIdUsuarioLogado()
+        {
+            try
+            {
+                var claimsIdentity = User.Identity as ClaimsIdentity;
+                var claim = claimsIdentity.FindFirst("Id");
+
+                if (claim != null)
+                    return Guid.Parse(claim.Value);
+
+                return Guid.Empty;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
